0493fa7c7fa6af20a4f4a6e5344adfb136576d88
t-nakatani <tomoya.12@icloud.com>
2025-02-04 19:31:39 +0900

    feat: orchester running scripts

--- post_new_art/main.py
+++ post_new_art/main.py
@@ -1,33 +1,27 @@
+import asyncio
+
 from app.agents.agent_factory import AgentFactory
-from app.agents.agent_persona import Personality
-from app.agents.persona.artist import Artist
+from app.agents.persona.orchesrator import Orchestrator
 from app.agents.persona.prompt_organizer import PromptOrganizer
-from app.config import pinata_config
-from app.externals.image_generater.dalle3_client import Dalle3Client
-from app.externals.ipfs.pinata_client import PinataClient
-from openai import OpenAI
+from app.dependencies.artist_agent import artist_agent_deps
+from app.dependencies.sns_marketer_agent import sns_marketer_agent_deps
+
+# def pinata_deps():
+#     return PinataClient(api_key=pinata_config.api_key, api_secret=pinata_config.api_secret)
 
 
-def artist_agent_deps(prompt_organizer: PromptOrganizer):
-    openai_client = OpenAI()
-    dalle3_client = Dalle3Client(openai_client=openai_client)
-    personality = Personality("example")
-    artist = Artist(
-        personality=personality,
-        prompt_organizer_agent=prompt_organizer,
-        image_generate_client=dalle3_client,
-    )
-    artist_agent = AgentFactory.create_from(artist)
-    return artist_agent
+async def main():
+    prompt_organizer = PromptOrganizer()
+    prompt_organizer_agent = AgentFactory.create_from(prompt_organizer)
+    artist_agent = artist_agent_deps(prompt_organizer_agent)
+    sns_marketer_agent = sns_marketer_agent_deps()
 
+    orchestrator = Orchestrator(artist_agent, sns_marketer_agent)
+    orchestrator_agent = AgentFactory.create_from(orchestrator)
 
-def pinata_deps():
-    return PinataClient(api_key=pinata_config.api_key, api_secret=pinata_config.api_secret)
-
-
-def main():
-    pass
+    result = await orchestrator_agent.run("generate a beautiful image of a cat and post it to X(Twitter)")
+    print(result.data)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
