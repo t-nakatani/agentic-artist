443ba7b460d5b77d0b269c54c0c8664a4037399f
t-nakatani <tomoya.12@icloud.com>
2025-02-09 20:16:46 +0900

    feat: improve sns posting prompt

--- post_new_art/app/agents/role/sns_marketer.py
+++ post_new_art/app/agents/role/sns_marketer.py
@@ -4,23 +4,26 @@ from app.agents.agent_persona import AgentPersona
 from app.externals.x.x_post_client import XPostClient
 from app.externals.x.x_post_data import XPostData
 from pydantic_ai.agent import RunContext
+from loguru import logger
 
 
 class SnsMarketer(AgentPersona):
     role = """
-    You are a SNS marketer agent. You are given a post and you need to post it to SNS.
+    You are a SNS marketer agent. You are given a file path and you need to post it to SNS.
+    Posting text is important. You need to create a posting text that is attractive and engaging.
+    Keep the posting text very short and concise.
     """
-    deps = list[Path]
+    deps = Path
     result_type = None
 
     def __init__(self, x_post_client: XPostClient):
         self.x_post_client = x_post_client
 
-    def post_new_art_to_x(self, ctx_with_file_paths: RunContext[list[Path]], post_text: str) -> str:
+    def post_new_art_to_x(self, ctx_with_file_path: RunContext[Path], post_text: str) -> str:
         """Post new arts to X."""
         post_data = XPostData(
             text=post_text,
-            media_paths=[str(file_path) for file_path in ctx_with_file_paths.deps],
+            media_paths=[str(ctx_with_file_path.deps)],
         )
-        # TODO: post_textのチューニング
+        logger.info(f"posting to X: {post_data.text}")
         return self.x_post_client.post_new_art(post_data)
