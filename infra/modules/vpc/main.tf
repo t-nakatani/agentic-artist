9c1f60fcdbdf669863e3d284d5be02fe9340fc5e
t-nakatani <tomoya.12@icloud.com>
2025-02-11 18:49:03 +0900

    feat: add vpc endpoint for private VPC

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -53,4 +53,20 @@ resource "aws_route_table_association" "public" {
   count          = 2
   subnet_id      = element(aws_subnet.public[*].id, count.index)
   route_table_id = aws_route_table.public.id
-} 
+}
+
+# S3用のVPCエンドポイントを追加
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.${data.aws_region.current.name}.s3"
+  
+  tags = {
+    Name = "${var.app_name}-s3-endpoint"
+  }
+}
+
+# VPCエンドポイントとルートテーブルの関連付け
+resource "aws_vpc_endpoint_route_table_association" "s3_public" {
+  route_table_id  = aws_route_table.public.id
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+}
