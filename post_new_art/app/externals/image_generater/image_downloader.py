496e9f0ed5a8026659bc1c3ea62a21ccba3314ae
t-nakatani <tomoya.12@icloud.com>
2025-02-11 15:47:20 +0900

    feat: extend timeout in image download

--- post_new_art/app/externals/image_generater/image_downloader.py
+++ post_new_art/app/externals/image_generater/image_downloader.py
@@ -15,7 +15,7 @@ class ImageDownloader:
             page = context.new_page()
             # page.set_extra_http_headers({"User-Agent": self.user_agent})
 
-            response = page.goto(url)
+            response = page.goto(url, timeout=120000)
             if response and response.status == 200:
                 Image.open(io.BytesIO(response.body())).save(output_path)
                 print(f"保存完了: {output_path}")
