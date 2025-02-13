c619d75bbf2916e7f8576c6575d9889ba536b70b
t-nakatani <tomoya.12@icloud.com>
2025-02-13 20:40:49 +0900

    wip

--- apis/post_new_art/app/externals/image_generater/image_downloader.py
+++ apis/post_new_art/app/externals/image_generater/image_downloader.py
@@ -3,15 +3,13 @@ import io
 from PIL import Image
 from playwright.sync_api import sync_playwright
 
-from app.config import playwright_config
-
 class ImageDownloader:
     def __init__(self, user_agent: str = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)"):
         self.user_agent = user_agent
 
     def download(self, url: str, output_path: str):
         with sync_playwright() as p:
-            browser = p.chromium.launch(executable_path=playwright_config.browser_executable_path)
+            browser = p.chromium.launch()
             context = browser.new_context(user_agent=self.user_agent)
             page = context.new_page()
             # page.set_extra_http_headers({"User-Agent": self.user_agent})
